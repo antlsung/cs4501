a43054239feda8a0c61322c4e440a38bbf5ccf5d
antlsung <antlsung@gmail.com>
2016-09-29 19:38:50 -0400

    working

--- web/web/urls.py
+++ web/web/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from . import views
+
 from django.conf.urls import url
 from django.contrib import admin
-from . import views
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', "home.index", name='home'),
+    url(r'^$', views.home, name='home'),
 ]
--- web/web/views.py
+++ web/web/views.py
@@ -4,5 +4,4 @@ from django.template import loader
 from django.shortcuts import render
 
 def home(request):
-    # template = loader.get_template('base.html')
     return render(request, 'base.html')
