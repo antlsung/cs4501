a594b5173f514bf67557797a4ddceae4e13f3aa0
antlsung <antlsung@gmail.com>
2016-10-25 15:43:36 -0400

    added password field

--- web/web/forms.py
+++ web/web/forms.py
@@ -3,7 +3,8 @@ from django import forms
 class CreateUser(forms.Form):
     name = forms.CharField(label='Your name', max_length=100)
     address = forms.CharField(label='Your address', max_length=150)
-    cart = forms.CharField(label='Your cart', max_length=150)
+    cart = forms.CharField(label='Your cart', max_length=100)
+    password = forms.CharField(label='Your password', max_length=100)
 
 class CreateShoe(forms.Form):
     shoe = forms.CharField(label='Shoe name', max_length=100)
