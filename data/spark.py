1a06a549d8ba53426d8cb8402737165d7871be92
antlsung <antlsung@gmail.com>
2016-12-12 18:22:09 -0500

    for ben

--- data/spark.py
+++ data/spark.py
@@ -1,4 +1,15 @@
 from pyspark import SparkContext
+import itertools
+
+def logic(x):
+	print(x)
+	y = []
+	for row in x:
+		temp = []
+		for a in range(0,row):
+			temp.append(a)
+		y.append(temp)
+	return y
 
 sc = SparkContext("spark://spark-master:7077", "PopularItems")
 
@@ -12,7 +23,8 @@ for pair1, pair2 in output:
 print ("PAIRS DONE")
 
 groups = pairs.groupByKey()
-print ("HERE  GROUPS>>>>>>>>>>>>>>>>>>>>>>>")
+# groups_distinct = groups[].distinct()
+print ("HERE GROUPS>>>>>>>>>>>>>>>>>>>>>>>")
 output = groups.collect()                          # bring the data back to the master node so we can print it out
 for pair1, pair2 in output:
 	print ("group part 1: %s part2: %s" % (pair1, pair2))
@@ -20,20 +32,47 @@ for pair1, pair2 in output:
 		print("id clicked on: %s" % (c))
 print ("GROUPS DONE")
 
-pages = groups.flatMap()
-print ("HERE  MAPS>>>>>>>>>>>>>>>>>>>>>>>")
+pages = groups.map(lambda pair: (pair[0], set(pair[1])))
+print ("HERE MAPS>>>>>>>>>>>>>>>>>>>>>>>")
 output = pages.collect()                          # bring the data back to the master node so we can print it out
 for pair1, pair2 in output:
-	print ("maps part 1: %s part2: %s" % (pair1, pair2))
+	print ("group part 1: %s part2: %s" % (pair1, pair2))
+	for c in pair2:
+		print("id clicked on: %s" % (c))
 print ("MAPS DONE")
 
-
-pages = pairs.map(lambda pair: (pair[1], 1))      # re-layout the data to ignore the user id
-print ("HERE  MAPS>>>>>>>>>>>>>>>>>>>>>>>")
-output = pages.collect()                          # bring the data back to the master node so we can print it out
+permutations = pages.map(lambda pair: (pair[0],list(itertools.combinations(pair[1], 2))))
+print ("HERE PERMUTATIONS>>>>>>>>>>>>>>>>>>>>>>>")
+output = permutations.collect()                          # bring the data back to the master node so we can print it out
+x=[]
 for pair1, pair2 in output:
-	print ("maps part 1: %s part2: %s" % (pair1, pair2))
-print ("MAPS DONE")
+	print ("pair part 1: %s part2: %s" % (pair1, pair2))
+	x.append(len(pair2))
+print ("PERMUTATIONS DONE")
+
+print ("SIZEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+# x = len(list(itertools.combinations(pair[1], 2)))
+print(x)
+arr = logic(x)
+print(arr)
+print ("HERE WORK>>>>>>>>>>>>>>>>>>>>>>>")
+arr2 = [0,1,2]
+blah = permutations.map(lambda output: (output[0],output[1][0]))
+total = blah.collect()
+for b1, b2 in total:
+	print ("B part 1: %s part2: %s" % (b1, b2))
+
+# flat = permutations.flatMap(lambda pair: (pair[0],list(itertools.combinations(pair[1], 2))) )
+# print ("HERE FLAT>>>>>>>>>>>>>>>>>>>>>>>")
+# output = flat.collect()                          # bring the data back to the master node so we can print it out
+# for pair1 in output:
+# 	print ("pair part 1: %s part2: %s" % (pair1, pair2))
+# print ("PERMUTATIONS DONE")
+#
+
+
+
+
 
 count = pages.reduceByKey(lambda x,y: x+y)        # shuffle the data so that each key is only on one worker
                                                   # and then reduce all the values by adding them together
